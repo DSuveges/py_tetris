f6b55dccb6aaba92131d0c5512a6bfaebc2efdce
dsuveges <suvi.dani@gmail.com>
2021-02-21 03:41:05 +0000

    Putting the screen together.

--- README.md
+++ README.md
@@ -2,10 +2,15 @@
 
 A tetris implementation in Python using pygame. This effort of mine is dedicated to the memory of [Jonas Neubauer](https://en.wikipedia.org/wiki/Jonas_Neubauer), the 7-time [Classic Tetris World champion](https://en.wikipedia.org/wiki/Jonas_Neubauer).
 
-## V.0.1
+## V.1.0
 
-![Current progress shown here](vid/tetris_v0.2.gif)
+![Current progress shown here](vid/tetris_v1.0.gif)
 
+1. Random method: double bag.
+2. Score calculated based on NES rules.
+3. Level calculated based on NES rules.
+4. Speed is kept constant across all levels. 
+5. Score/next tetromino/level/cleared rows shown on side panel.
 
 ### Requirements
 
@@ -13,13 +18,10 @@ A tetris implementation in Python using pygame. This effort of mine is dedicated
 * [pygame](https://www.pygame.org/news)
 * [numpy](https://numpy.org/)
 
-### Specification
-
-Tetris rules are implemented defied for [NES tetris](https://tetris.fandom.com/wiki/Tetris_Wiki) (more or less). 
 
 ### TODOs
 
-* Implement scoring according to NES rules.
+* Side movement of the tetrominos not cool.
 * Implement [wall-kick](https://tetris.wiki/Super_Rotation_System#Wall_Kicks)
 * Develop some db to store high-scores.
 * Develop two-player front end.
--- tetris.py
+++ tetris.py
@@ -7,7 +7,7 @@ sys.path.append("/Users/dsuveges/repositories/py_tetris/modules")
 from config import Configurations
 from Bag import Bag
 from Matrix import Matrix
-from Plot import MatrixPlot, BackgroundPlot, NextElementPlot
+from Plot import MatrixPlot, BackgroundPlot, NextElementPlot, LabelsPlot
 from Scoring import Score
 
 
@@ -23,8 +23,6 @@ random_seed = Configurations.random_seed
 tetromino_colors = Configurations.tetronimo_colors
 matrix_background_color = Configurations.matrix_background_color
 matrix_offset = Configurations.matrix_offset
-next_element_offset = Configurations.next_element_offset
-next_element_size = Configurations.next_element_size
 
 # Initializing 
 bag = Bag(tetronimos,random_method=random_method, seed=random_seed)
@@ -34,10 +32,8 @@ screen_size = Configurations.screen_dimensions
 display = pygame.display.set_mode(screen_size)
 display.fill(Configurations.display_color)
 bg = BackgroundPlot(display, Configurations)
-bg.draw_matrix_frame()
-bg.draw_next_element_frame()
-show_next = NextElementPlot(tetromino_colors, brick_size, matrix_background_color, display, next_element_size, next_element_offset)
-
+show_next = NextElementPlot(tetromino_colors, brick_size, matrix_background_color, display, Configurations.next_element)
+labels = LabelsPlot(display, Configurations)
 
 # Set timing:
 time_elapsed_since_last_action = 0
@@ -51,6 +47,12 @@ m = Matrix(dimensions)
 
 # Initialzing score object
 scoring = Score(Configurations.scoring, level=1)
+
+# Filling panels:
+labels.update_player('Player 1')
+labels.update_score(scoring.get_score())
+labels.update_level(scoring.get_level())
+labels.update_rows(scoring.get_rows())
 
 
 # 
@@ -90,19 +92,35 @@ while True:
 
     # dt is measured in milliseconds, therefore 250 ms = 0.25 seconds
     if time_elapsed_since_last_action > 700:
-        m.move_down()
+        down_ret = m.move_down()
+        if down_ret == 'game over':
+            break
 
         # reset it to 0 so you can count again
         time_elapsed_since_last_action = 0 
 
     # After every tick, we check for completed rows and update:
     completed_rows = m.test_for_complete_row()
-    scoring.rows_cleared(completed_rows)
 
-    print(f'score: {scoring.get_score()}, level: {scoring.get_level()}, rows cleared: {scoring.get_lines()}\r')
-
+    if completed_rows > 0:
+        scoring.rows_cleared(completed_rows)
+        labels.update_score(scoring.get_score())
+        labels.update_level(scoring.get_level())
+        labels.update_rows(scoring.get_rows())
+    
 
     # Update screen:
     m_plot.draw_matrix(m.get_state())
     pygame.display.flip()
 
+
+
+    # If we exit from the loop, it means the game is over:
+    # pygame.draw.rect(self.display, self.frame_color, rect)
+    my_image = pygame.Surface(screen_size, pygame.SRCALPHA)
+    BLUE = (0, 0, 255, 255)
+    pygame.draw.rect(my_image, BLUE, my_image.get_rect())
+
+
+while True:
+    x = 1
